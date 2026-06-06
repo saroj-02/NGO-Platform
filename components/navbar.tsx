04b63a40579389c45c96b6c5480a5142931cef54
v0agent <it+v0agent@vercel.com>
2026-06-06 15:24:59 +0000

    feat: update navbar title and switch to dev type routes\n\nUpdated Navbar title and changed type reference to dev routes

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -25,7 +25,7 @@ export function Navbar() {
             <Heart className="size-5" fill="currentColor" />
           </span>
           <span className="font-heading text-lg font-extrabold tracking-tight text-foreground">
-            HopeBridge
+            SG SAROJ GROUPS
           </span>
         </Link>
 
